68fa48f2e76a442c8a3577ea7ea56dc7d8d7a993
Saveen <63491393+saveen123@users.noreply.github.com>
2025-09-20 14:37:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 const express = require('express');
-const path = require('path'); // built-in module
+const cors = require('cors');
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt')
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-// If your HTML file is in the same folder as this script
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'admin',
+  database: 'mydb',
+  password: 'admin'
 });
 
-// Or, if you put HTML in a 'public' folder, you can serve it statically:
-// app.use(express.static('public'));
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    process.exit(1); // Exit if connection fails
+  }
+  console.log('Connected to MySQL database');
 });
+
+async function compareHash(data){
+    
+}
+
+async function hashdData(data){
+    try{
+        const res = await bcrypt.hash(data, 10);
+        return res;
+    }catch(err){
+        console.log('Hashing error')
+    }
+}
+
+function findUser(email){
+    connection.query('SELECT * FROM `user` WHERE email = ?',[email],(err,res)=>{
+        return res[0];
+    })
+}
+
+function insertData(email, pass){
+    connection.query(
+        'INSERT INTO `user` (email,password) VALUES (?,?)',[email, pass],
+        function (err, results, fields) {
+        
+        }
+    );
+
+}
+
+
+app.get('/',(req,res)=>{
+    const {email, password} = req.body;
+    res.send('api working')
+    console.log(findUser(email))
+    
+})
+app.listen(3000,()=>{
+    console.log('server up and running')
+})
+
